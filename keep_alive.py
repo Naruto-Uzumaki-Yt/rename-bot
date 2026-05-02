6e4b516f0abe0664bf3220c12f11e57e03ce893c
Naruto-Uzumaki-Yt <narutouzumaki3016day@gmail.com>
2026-05-02 19:22:22 +0530

    keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,26 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is Alive 🔥"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    Thread(target=run).start()
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
